6878b26f92dbffff4ae6a7856d41f2a6b8338bc8
kevinstiebich <kevin.stiebich@gmail.com>
2026-08-13 16:43:16 +0200

    C Teil 4 beginnen und .txt erstellen

---
+++ todo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdlib.h> // für dynamischen Speicher?
+
+int main(int argc, char *argv[]) {
+    FILE *f = fopen("todo.txt", "r+");
+}
