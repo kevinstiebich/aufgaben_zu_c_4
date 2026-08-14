9d4ed55afd76c670c154532c9026aafdaed9d158
kevinstiebich <kevin.stiebich@gmail.com>
2026-08-14 14:18:03 +0200

    opt hinzugefügt - noch unbrauchbar

--- todo.c
+++ todo.c
@@ -4,5 +4,17 @@
 #include <stdlib.h> // für dynamischen Speicher?
 
 int main(int argc, char *argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "a:h") != 1)) {
+        switch (opt) {
+            case 'h':
+                printf("usage: todo [-a text] [-e text item-no] [-d item-no]\n");
+                printf("            [-c item-no] [-u item-no] [-l] [-U] [-C] [-h]");
+                break;
+        }
+    }
+
+    return 0;
     FILE *f = fopen("todo.txt", "r+");
 }
