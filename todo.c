b0efd600521faffbaeb0e533b8fc2387aab7ecca
kevinstiebich <kevin.stiebich@gmail.com>
2026-08-19 17:22:48 +0200

    createTempFile() Funktion bauen

--- todo.c
+++ todo.c
@@ -4,12 +4,45 @@
 #include <stdlib.h>
 #include <getopt.h>
 
+FILE *createTempFile(char *buffer, size_t bufferSize, const char *filename) {
+    int written = snprintf(buffer, bufferSize, "%sXXXXXX", filename);
+
+    if (written < 0 || (size_t)written >= bufferSize) {
+        fprintf(stderr, "Fehler: Puffer zu klein für den Dateinamen.\n");
+        return NULL;
+    }
+
+    int fd = mkstemp(buffer);
+
+    if (fd == -1) {
+        perror("mkstemp fehlgeschlagen");
+        return NULL;
+    }
+
+    FILE *fp = fdopen(fd, "w+");
+
+    if (fp == NULL) {
+        perror("fdopen fehlgeschlagen");
+        close(fd); // Deskriptor schließen, falls fdopen fehlschlägt
+        return NULL;
+    }
+
+    return fp;
+}
+
 int main(int argc, char *argv[]) {
     int opt;
     char *a = NULL;
     int d = 0;
     int c = 0;
     int u = 0;
+
+    char nameBuffer[256];
+    const char *basename = "todo_";
+
+    FILE *temp = createTempFile(nameBuffer, size_of(nameBuffer), basename);
+
+    FILE *f = fopen("todo.txt", "r");
 
     while ((opt = getopt(argc, argv, "a:e:d:c:u:lUCh")) != -1) {
         switch (opt) {
@@ -20,12 +53,30 @@ int main(int argc, char *argv[]) {
                 // text und nr, hier mit optarg und opting dann machen
             case 'd':
                 d = atoi(optarg);
+
+                if (d < 1) {
+                    fprintf(stderr, "Fehler: Die Item-Nummer kann nicht niedriger als 1 sein.\n");
+                    return EXIT_FAILURE;
+                }
+
                 break;
             case 'c':
                 c = atoi(optarg);
+
+                if (c < 1) {
+                    fprintf(stderr, "Fehler: Die Item-Nummer kann nicht niedriger als 1 sein.\n");
+                    return EXIT_FAILURE;
+                }
+
                 break;
             case 'u':
                 u = atoi(optarg);
+
+                if (u < 1) {
+                    fprintf(stderr, "Fehler: Die Item-Nummer kann nicht niedriger als 1 sein.\n");
+                    return EXIT_FAILURE;
+                }
+
                 break;
             case 'l':
             case 'U':
@@ -33,10 +84,12 @@ int main(int argc, char *argv[]) {
             case 'h':
                 printf("usage: todo [-a text] [-e text item-no] [-d item-no]\n");
                 printf("            [-c item-no] [-u item-no] [-l] [-U] [-C] [-h]\n");
-                break;
+                return EXIT_SUCCESS;
+            case '?':
+                return EXIT_FAILURE;
             default:
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
